62af3b965af4ba202e52c2a5b303762a9ebe7c17
Raman Nasovich <raman.nasovich@thomsonreuters.com>
2018-05-10 11:19:36 +0300

    added async implementation for import function without library usage

--- file-handling/importer.js
+++ file-handling/importer.js
@@ -1,5 +1,6 @@
 import csv from 'csvtojson';
 import fs from 'fs';
+import readline from 'readline';
 
 export class Importer {
     import(path) {
@@ -7,6 +8,35 @@ export class Importer {
             csv().fromFile(path).on('end_parsed', (json) => {
                 resolve(json);
             });
+        });
+    }
+    //async implementation without library usage
+    importAsync(path) {
+        return new Promise((resolve, reject) => {
+
+            let objects = [];
+            let columns;
+
+            const reader = readline.createInterface(fs.createReadStream(path));
+
+            reader.on('line', (line) => {
+                const values = line.split(',');
+                //if columns is undefined then first line is processing 
+                if (!columns) {
+                    columns = values;
+                } else {
+                    const obj = {};
+                    columns.forEach((column, index) => {
+                        obj[column] = values[index];
+                    });
+                    objects.push(obj);
+                }
+            });
+
+            reader.on('close', () => {
+                resolve(JSON.stringify(objects));
+            });
+
         });
     }
     //csvtojson doesn't provide sync ways to convert csv to json
