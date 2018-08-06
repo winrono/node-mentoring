6d2fa797879e4055e60f8611ec6b5da6ef418135
Roman <romanmail.by@gmail.com>
2018-08-06 05:15:58 +0300

    changed html-server implementation

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -1,13 +1,13 @@
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
+import url from 'url';
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //using readFileSync as described in homework
-    // const content = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-    // res.end(content);
-    const rs = fs.createReadStream(path.resolve(__dirname, './index.html'));
-    rs.pipe(res);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    let query = url.parse(req.url, true).query;
+    let content = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+    content = content.replace('{message}', query.message);
+    res.end(content);
 
 }).listen(3000);
